0c1ce999c4779d6c92469940ea70b3a3ea81f6f7
shank <shank@DESKTOP-5QGUCI3>
2019-07-28 12:41:58 +0530

    commit at branch z

---
+++ src/Order13.java
@@ -0,0 +1,18 @@
+
+public class Order13 {
+int id;
+int name;
+int price;
+public Order13(int id,int name,int price)
+{
+	this.id=id;
+this.name=name;
+this.price=price;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
